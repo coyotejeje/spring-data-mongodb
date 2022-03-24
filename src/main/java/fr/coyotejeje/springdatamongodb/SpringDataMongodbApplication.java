fe5a1012cf24abbc45817eb7068975bcd98e4984
jerome <micl.jerome@gmail.com>
2022-03-24 15:07:40 +0100

    completed all course

--- src/main/java/fr/coyotejeje/springdatamongodb/SpringDataMongodbApplication.java
+++ src/main/java/fr/coyotejeje/springdatamongodb/SpringDataMongodbApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,17 +21,23 @@ public class SpringDataMongodbApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(
+            StudentRepository repository,
+            MongoTemplate mongoTemplate
+    ) {
        return args -> {
            Address address = new Address(
                    "France",
-                   "Lyon",
-                   "69000"
+                   "Aix-en-Provence",
+                   "13090"
            );
+
+           String email = "john@gmail.com";
+
            Student student = new Student(
                    "John",
                    "Doe",
-                   "john.doe@gmail.com",
+                   email,
                    Gender.MALE,
                    address,
                    List.of("Computer Science", "Maths"),
@@ -36,8 +45,34 @@ public class SpringDataMongodbApplication {
                    LocalDateTime.now()
            );
 
-           repository.insert(student);
+           // usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
+           repository.findStudentByEmail(email)
+                           .ifPresentOrElse(s -> {
+                               System.out.println(student + " already exists");
+                           }, () -> {
+                               System.out.println("inserting student " + student);
+                               repository.insert(student);
+                           });
+
+           //repository.insert(student);
        };
+    }
+
+    private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+
+        List<Student> students = mongoTemplate.find(query, Student.class);
+        if (students.size() > 1) {
+            throw new IllegalStateException("found many students with email " + email);
+        }
+
+        if (students.isEmpty()) {
+            System.out.println("inserting student " + student);
+            repository.insert(student);
+        } else {
+            System.out.println(student + " already exists");
+        }
     }
 
 }
--- src/main/java/fr/coyotejeje/springdatamongodb/Student.java
+++ src/main/java/fr/coyotejeje/springdatamongodb/Student.java
@@ -2,6 +2,7 @@ package fr.coyotejeje.springdatamongodb;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -11,11 +12,16 @@ import java.util.List;
 @Data
 @Document
 public class Student {
+
     @Id
     private String id;
+
     private String firstName;
     private String lastName;
+
+    @Indexed(unique = true)
     private String email;
+
     private Gender gender;
     private Address address;
     private List<String> favouriteSubjects;
